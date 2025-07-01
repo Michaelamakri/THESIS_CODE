c81b4c38c033d4f8e94eecf6ac9ab15d1478e901
Michaelamakri <76743932+Michaelamakri@users.noreply.github.com>
2025-07-01 12:22:32 +0300

    Update diplomatiki_3.py

--- diplomatiki_3.py
+++ diplomatiki_3.py
@@ -1,109 +1,130 @@
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-import csv
+import csv 
+
 No=1e-10
+SINR_TARGET=30
+initial_distances = [] 
 current_population = [] 
 
+
 def dbm_to_mw(dbm):
-    y=10**(dbm/10)
+    y=(10**(dbm/10))
     return y
+def mw_to_dbm(mw):
+    y=10*np.log10(mw)
+    return y
+#Η συνάρτηση fitness περιγράφει την συνάρτηση που αφορά το SINR και τα ονοματα των
+#παραμέτρων προκύπτουν από την μαθηματική σχέση
 def fitness(individual):
     global current_population
-    Pc_dbm, Pd_dbm, htx2rx, dtx2rx, dd2d, a, dc2d, hd2d, hc2d = individual
-
+    global dtx2rx,dd2d,dc2d# για να μπορουμε μετα την 2η γενια να έχουμε ίδιες αποστάσεις στο μοντέλο
+    Pc_dbm, Pd_dbm, htx2rx, dtx2rx, dd2d, a, dc2d, hd2d, hc2d = individual#κάθε individual έχει αυτές τις τιμές
     Pd = dbm_to_mw(Pd_dbm)
     Pc = dbm_to_mw(Pc_dbm)
-
-    signal = Pd * abs(htx2rx) ** 2 * dtx2rx ** (-a)
-
+    signal = Pd*abs(htx2rx)**2*dtx2rx**(-a)
+    
     interference = 0
-    for other in current_population:
+    for other in current_population:#υπολογισμός παρεμβολών από τους χρήστες
         if other == individual:
             continue
-        Pd_j = dbm_to_mw(other[1])
-        hd2d_j = other[7]
-        dd2d_j = other[4]
-        interference += Pd_j * abs(hd2d_j) ** 2 * dd2d_j ** (-a)
+        Pcother=dbm_to_mw(other[0])
+        Pdother=dbm_to_mw(other[1])
+        hd2dother=other[7]
+        dd2dother=other[4]
+        interference =interference+Pdother* (abs(hd2dother)**2)* (dd2dother**(-a))
 
-    interference += Pc * abs(hc2d) ** 2 * dc2d ** (-a)
+    interference =interference+Pc*(abs(hc2d)**2)*(dc2d**(-a))
+    sinr=(signal)/(interference+No)
 
-    sinr = signal / (interference + No)
-    sinr_db = 10 * np.log10(sinr)
-
+    sinr_db = 10*np.log10(sinr)
     return sinr_db
+
 def create_initial_population(size):
     population=[]
-
     for i in range(size):
-        Pc = random.uniform(10,24)
-        Pd = random.uniform(10,24)
-
-        htx2rx = np.random.rayleigh(scale=0.5)
-        hd2d = np.random.rayleigh(scale=0.5)
-        hc2d = np.random.rayleigh(scale=0.5)
-        dtx2rx=random.uniform(10,100)
-        dd2d=random.uniform(10,100)
+        Pc_dBm =-40
+        Pd_dBm =23
+        htx2rx = max(np.random.rayleigh(scale=2), 0.5)
+        hd2d=max(np.random.rayleigh(scale=2), 0.5)
+        hc2d=max(np.random.rayleigh(scale=2), 0.5)
+        dtx2rx=np.random.uniform(10,100)
+        dd2d=np.random.uniform(10,100)
         a=2.75
-        dc2d=random.uniform(10,100)
-        individual=[Pc,Pd,htx2rx,dtx2rx,dd2d,a,dc2d,hd2d,hc2d]
-        population.append(individual)
+        dc2d=np.random.uniform(10,100)
+        individual=[Pc_dBm,Pd_dBm,htx2rx,dtx2rx,dd2d,a,dc2d,hd2d,hc2d]
+        population.append(individual)       
     return population
 
-def selection(population,fitnesses,tournament_size=3):
+def selection(population,fitnesses,tournament_size=4):
     selected=[]
-    tournament_size = min(len(population),tournament_size)
-    for i in range(len(population)):
+    x=len(population)
+    tournament_size = min(x,tournament_size)#επιλογή tournament selection 
+    for i in range(x):
         tournament=random.sample(list(zip(population, fitnesses)), tournament_size)
         winner = max(tournament, key=lambda x: x[1])[0]
         selected.append(winner)
     return selected
-# Crossover function
+
 def crossover(parent1, parent2):
     genes = random.random()
     child1=[]
     child2=[]
-    y1=zip(parent1,parent2)
-    for i,j in y1:
-        child1.append(genes*i+(1-genes)*j)
-        child2.append(genes*j+(1-genes)*i)
+    for i in range(len(parent1)):
+        if i in [3, 4,5, 6]: #για την διατηρηση σταθερών αποστάσεων τις αποκλείουμε από τοcrossover
+            child1.append(parent1[i])
+            child2.append(parent1[i])
+        else:
+            child1.append(genes*parent1[i]+(1-genes)*parent2[i])#γονιδια
+            child2.append(genes*parent2[i]+(1-genes)*parent1[i])
     return child1, child2
-
-def mutation(individual,mutation_rate, lower_bound, upper_bound):
+    
+def mutation(individual,mutation_rate,bestfit):
+    
     for i in range(len(individual)):
-        if random.random() < mutation_rate:
+        if i in [3, 4,6]:
+            continue
+        if  random.random()< mutation_rate:
             factor= random.uniform(0.9, 1.1)
-            individual[i]=individual[i]*factor
-            if (i==2) or (i==7) or (i==8):
-                individual[i] = max(min(individual[i],5.0),0.1)
-            elif (i==3) or (i==4) or (i==6):
-                individual[i]=max(min(individual[i],100),5)
-            elif (i==0) or (i==1):
-                individual[i]=max(min(individual[i],24),10)
+            if (i==0) :
+                individual[i]=individual[i]*factor
+            elif (i==1):
+                individual[i]=individual[i]*factor#έλεγχος ισχύος
+                if (bestfit<SINR_TARGET):
+                    k=1
+                    individual[1]=min(individual[1]+k,40)
+                if (bestfit>SINR_TARGET):
+                    k=1
+                    individual[1]=individual[1]-k
     return individual
+
 
 def main():
     global current_population;
     generations=100
     mutation_rate=0.1
-    lower_bound=0.1
-    upper_bound=5.0
     best_fitnesses=[]
     
-    population_ex=create_initial_population(100)
+    population_ex=create_initial_population(50)
+      
     open("best_fits.txt",'w').close()
 
     for i in range(generations):
         current_population=population_ex
         fitnesses_ex = [fitness(individual) for individual in population_ex]
         best_fitness = max(fitnesses_ex)
+        print("Generation:",i,"bestfitness:",best_fitness)
         best_fitnesses.append(best_fitness)
+        #αποθήκευση σε txt αρχεία για να μπορούν να διαβαστουν τα αποτελεσματα για κάθε γενιά
         filename = "generation"+str(i)+".txt"
         with open(filename,'w') as file:
             file.write("generation"+str(i+1)+":\n")
             
             for j,individual in enumerate(population_ex):
                 f=fitness(individual)
+                if i in [1,99]:
+                    print(individual[0],individual[1],individual[6])
                 file.write("Individual"+str(j+1)+":\n")
                 file.write("Pc(dbm):"+str(individual[0])+"\n")
                 file.write("Pd(dbm):"+str(individual[1])+"\n")
@@ -123,12 +144,12 @@ def main():
             writer.writerow(["Individual", "Fitness"])
             for j, fitness_value in enumerate(fitnesses_ex):
                 writer.writerow([j + 1,fitness_value])
-                
-
+        #αποθήκευση σε csv για επεξεργασία από matlab
         with open("bestfits.csv", mode='w', newline='') as csvfile:
             writer = csv.writer(csvfile)
             for value in best_fitnesses:
-                writer.writerow([value])  
+                writer.writerow([value])
+        
         elite_index = np.argmax(fitnesses_ex)
         elite_individual = population_ex[elite_index]
         selected=selection(population_ex,fitnesses_ex)
@@ -142,9 +163,35 @@ def main():
                 next_gen.extend([child_ex1,child_ex2])
             else:
                 next_gen.append(parent_ex1)
+        
 
-        next_gen = [mutation(child, mutation_rate, lower_bound, upper_bound) for child in next_gen]
+        next_gen = [mutation(child, mutation_rate,best_fitness) for child in next_gen]
         next_gen[0]=elite_individual 
         population_ex=next_gen
+    #Plot
+    
+
+    best_individual = population_ex[np.argmax([fitness(ind) for ind in population_ex])]  
+    distances = np.linspace(10, 100, 200)  
+    sinrs = []
+
+    for d in distances:
+        modified = best_individual.copy()
+        modified[3] = d 
+        sinr_val = fitness(modified)
+        sinrs.append(sinr_val)
+
+    
+    plt.figure(figsize=(10, 6))
+    plt.plot(distances, sinrs, label="SINR vs dtx2rx", color='blue')
+    plt.xlabel("Απόσταση Πομπού-Δέκτη (dtx2rx) [m]")
+    plt.ylabel("SINR(dB)")
+    plt.title("Εξέλιξη του SINR ως προς την Απόσταση Πομπού-Δέκτη")
+    plt.grid(True)
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+                    
 if __name__ == "__main__":
     main()
+
